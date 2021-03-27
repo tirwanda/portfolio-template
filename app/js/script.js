7bd514334ffe2192a1b86f7bda5f2444ec707ae7
Edho Dwi Tirwanda <edhodwitirwanda@gmail.com>
2021-03-27 21:51:00 +0700

    Add Animation Scroll Up Button

--- app/js/script.js
+++ app/js/script.js
@@ -28,6 +28,16 @@ $(document).ready(function () {
 		} else {
 			$('.navbar').removeClass('sticky');
 		}
+
+		if (this.scrollY > 500) {
+			$('.scroll-up').addClass('show');
+		} else {
+			$('.scroll-up').removeClass('show');
+		}
+	});
+
+	$('.scroll-up').click(function () {
+		$('html').animate({ scrollTop: 0 });
 	});
 });
 
